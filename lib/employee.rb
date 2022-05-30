627e7b28cc71e1cf397addae639ab0d336b1e258
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-30 17:55:13 -0400

    Implement before_save callback for password generation

--- lib/employee.rb
+++ lib/employee.rb
@@ -7,7 +7,7 @@ class Employee < ActiveRecord::Base
   validates :hourly_rate, numericality: { greater_than_or_equal_to: 40, less_than_or_equal_to: 200, message: 'Hourly rate must be between 40 and 200 inclusive.' }
   validates :store_id, presence: true
 
-  before_create :auto_generate_password
+  before_save :auto_generate_password
    
   private 
 
