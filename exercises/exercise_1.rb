6d7430002a12cb9d7cd3ed460cec85715a2b2af3
Jennifer Manwell <manwell.fortier@gmail.com>
2022-05-30 10:26:40 -0400

    Complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,12 @@
 require_relative '../setup'
+require_relative '../lib/store'
 
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+### Exercise 1: Create 3 stores
+
+#1. Use Active Record's `create` class method multiple times to create 3 stores in the database:
+Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
